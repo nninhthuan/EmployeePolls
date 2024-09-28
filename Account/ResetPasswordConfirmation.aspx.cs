2d7ce981fa8923dd55442d5cd8a224310999ec8c
Nguyen Ninh Thuan <nninhthuan211@gmail.com>
2024-09-28 20:08:06 +0700

    Add project files.

---
+++ Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EmployeePolls.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeePolls.Global" Language="C#" %>
